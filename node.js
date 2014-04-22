c83d88795c8c6c31186fe091dfa8223fa94f0e0e
Alex Lawrence <info@alex-lawrence.com>
2014-04-22 11:23:58 +0200

    Fix path for requiring in node.

--- node.js
+++ node.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var sysPath = require('path');
 
-module.exports = fs.readFileSync(sysPath.join(__dirname, 'commonjs-loader.js'), 'utf8');
+module.exports = fs.readFileSync(sysPath.join(__dirname, 'commonjs-require.js'), 'utf8');
