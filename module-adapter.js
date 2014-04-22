08f43c1382a3a3b03c4ff4961aa9b65b3e61b638
Alex Lawrence <info@alex-lawrence.com>
2014-04-22 11:25:54 +0200

    Add module adapter for better node module support.

---
+++ module-adapter.js
@@ -0,0 +1,11 @@
+window.require.register({'module': function(exports, require, module) {
+
+  var self = {};
+  self._load = window.require;
+  window.require = function() {
+    return self._load.apply(window, arguments);
+  };
+  self._cache = require._cache;
+  module.exports = window;
+
+}});
